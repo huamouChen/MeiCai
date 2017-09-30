ea9628150ee3d7bc456e5068603e576503f95c11
风华正茂回谋一笑 <chenhm4444@gmail.com>
2017-09-30 16:46:58 +0800

    基本界面搭建

---
+++ MeiCai/MeiCai/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MeiCai
+//
+//  Created by 陈华谋 on 2017/9/29.
+//  Copyright © 2017年 陈华谋. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
